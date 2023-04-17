573cd12c160e743a2c92c3c7f7870bb3bf56a916
Mike Menko <menko.mihail@gmail.com>
2023-04-17 11:55:58 +0200

    Setup correct and editable temporary bot URL address

--- src/main/java/info/mastera/bot/client/BorderStateBotClient.java
+++ src/main/java/info/mastera/bot/client/BorderStateBotClient.java
@@ -6,9 +6,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "BorderStateBotApi", url = "http://localhost:8088")
+@FeignClient(name = "BorderStateBotApi", url = "${border-waiting-area.bot.url}")
 public interface BorderStateBotClient {
-
 
     @PostMapping(
             produces = MediaType.APPLICATION_JSON_VALUE,
