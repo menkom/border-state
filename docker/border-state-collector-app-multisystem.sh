10ee40c205666f6c6ec1595bc40de044c222b03e
Mike Menko <menko.mihail@gmail.com>
2023-05-01 11:27:48 +0200

    Build multiplatform (amd64/arm64) image and push it to docker registry

---
+++ docker/border-state-collector-app-multisystem.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+docker rm border-state-collector-app
+
+docker buildx create --use
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t menkomihail/border-state-collector-app:v1 \
+  -f app/Dockerfile \
+  ../ \
+  --push
