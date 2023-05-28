e0e35b44b79782cc68aa666de821ea15d845e561
Mike Menko <menko.mihail@gmail.com>
2023-05-28 19:41:04 +0200

    Logs collecting in OpenSearch+Dashboard using Fluentd

--- docker/border-state-collector-app-multisystem.sh
+++ docker/border-state-collector-app-multisystem.sh
@@ -6,7 +6,7 @@ docker buildx create --use
 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  -t menkomihail/border-state-collector-app:v1.0.2 \
+  -t menkomihail/border-state-collector-app:v1.0.3 \
   -f app/Dockerfile \
   ../ \
   --push
